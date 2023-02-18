58732d409ee5fe3589896f89e5bea6a6d24fb460
Warpten <vertozor@gmail.com>
2023-02-18 02:21:56 +0100

    Try to force C++20

--- src/tactmon/backend/db/PreparedStatement.hpp
+++ src/tactmon/backend/db/PreparedStatement.hpp
@@ -46,11 +46,11 @@ namespace backend::db {
         static auto ExecuteOne(pqxx::transaction_base& transaction, std::tuple<Args...> args)
             -> std::optional<typename QUERY::projection_type>
         {
-            pqxx::row resultSet = ExecuteOne_(transaction, args);
-            if (resultSet.empty())
+            pqxx::result resultSet = Execute_(transaction, args);
+            if (resultSet.size() != 1)
                 return std::nullopt;
 
-            return typename QUERY::projection_type { resultSet };
+            return typename QUERY::projection_type { resultSet[0] };
         }
 
         template <typename... Args>
